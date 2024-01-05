22554f2f53c58e652fbbd15517588f7c34c3a4cc
Chukwuemeka <mekastans@gmail.com>
2024-01-05 23:20:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# [mekazstan.github.io](https://mekazstan.github.io/)
+
+Link: [https://mekazstan.github.io](https://mekazstan.github.io/)
+
+Personal Portfolio Website Hosted with Github Pages
+
+<img width="100%" src=./images/frontpage_hero.png>
+
+## Page Content
+- [**Homepage**](https://mekazstan.github.io/) : Personal introduction and overview, Links to Activity Updates and Socials
+- [**About**](https://mekazstan.github.io/about.html/) : Comprehensive Personal and Professional Information, including but not limited to; 
+  - Services (Personal and Professional)
+  - Contact details
+  - Social Media profiles, 
+  - Personal Projects overview
+  - Project Collborations Overview
+  - Open-source Contributions, etc
+- [**Support**](https://mekazstan.github.io/support): Crypto Support Page
+- [**Extras**](https://mekazstan.github.io/extras.html): Coming soon! 
+- [**Custom 404 Page**](https://mekazstan.github.io/404.html/): Entertaining and Calming Error Message, Link to Home Page, and Link to Report Issue through a Mail or Social Media account
+- Link to latest version of my [**Resume**](https://raw.githubusercontent.com/mekazstan/Resume/master/_Chukwuemeka Resume.pdf)
+
+## Personal Details
+- **Name**: Chukwuemeka Asogwa
+- **Title**: Engr.
+- **Profession**: Data Engineer / Analyst
+- **Nationality**: Nigeria <img src="https://github.com/csmoore/country-flag-icons/raw/master/country-flags-4x3-png/ng.png" alt="drawing" width="15"/>
+
+
+## Page Technology Stack
+- HTML / CSS
+- Boostrap (CSS, JS and Fonts)
+- Javascript
+- FontAwesome
+
+
+## Maitainer:
+- Chukwuemeka Asogwa <mekastans@gmail.com>
---
+++ js/main.js
@@ -0,0 +1,295 @@
+// THEME JS
+// define variables to hold the classname for the 2 themes
+// define variable to hold reference to the switch button
+let theme_dark = 'theme-dark';
+let theme_light = 'theme-light';
+let theme_switch = document.getElementById("theme-switch");
+
+// function to set to a given theme/color-scheme
+function setTheme(themeName){
+    console.log("setting theme to ", themeName);
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+    if (themeName === theme_dark){
+        theme_switch.classList.add("bi-sun");
+        theme_switch.classList.remove("bi-moon");
+    } else {
+        theme_switch.classList.add("bi-moon");
+        theme_switch.classList.remove("bi-sun");
+    }
+}
+
+// function to toggle between light and dark theme
+function toggleTheme(){
+    if (localStorage.getItem('theme') === theme_dark){
+        setTheme(theme_light);
+    } else {
+        setTheme(theme_dark);
+    }
+}
+
+// Immediately invoke the function to set the theme on initial load
+(function (){
+    if (localStorage.getItem('theme') === theme_dark){
+        setTheme(theme_dark);
+    } else {
+        setTheme(theme_light);
+    }
+})();
+
+
+// Frequently asked question section script
+let faq = document.getElementsByClassName("faq-question");
+let faqs = document.getElementsByClassName("faq-question");
+
+let i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+
+/**
+* Javascript used for Navbar gotten from...
+* Template Name: Medilab - v4.7.1
+* Template URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+
+(function() {
+    "use strict";
+  
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+      el = el.trim()
+      if (all) {
+        return [...document.querySelectorAll(el)]
+      } else {
+        return document.querySelector(el)
+      }
+    }
+  
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+      let selectEl = select(el, all)
+      if (selectEl) {
+        if (all) {
+          selectEl.forEach(e => e.addEventListener(type, listener))
+        } else {
+          selectEl.addEventListener(type, listener)
+        }
+      }
+    }
+  
+    /**
+     * Easy on scroll event listener 
+     */
+    const onscroll = (el, listener) => {
+      el.addEventListener('scroll', listener)
+    }
+  
+    /**
+     * Navbar links active state on scroll
+     */
+    let navbarlinks = select('#navbar .scrollto', true)
+    const navbarlinksActive = () => {
+      let position = window.scrollY + 200
+      navbarlinks.forEach(navbarlink => {
+        if (!navbarlink.hash) return
+        let section = select(navbarlink.hash)
+        if (!section) return
+        if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+          navbarlink.classList.add('active')
+        } else {
+          navbarlink.classList.remove('active')
+        }
+      })
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+  
+    /**
+     * Scrolls to an element with header offset
+     */
+    const scrollto = (el) => {
+      let header = select('#header')
+      let offset = header.offsetHeight
+  
+      let elementPos = select(el).offsetTop
+      window.scrollTo({
+        top: elementPos - offset,
+        behavior: 'smooth'
+      })
+    }
+  
+    /**
+     * Toggle .header-scrolled class to #header when page is scrolled
+     */
+    let selectHeader = select('#header')
+    let selectTopbar = select('#topbar')
+    if (selectHeader) {
+      const headerScrolled = () => {
+        if (window.scrollY > 100) {
+          selectHeader.classList.add('header-scrolled')
+          if (selectTopbar) {
+            selectTopbar.classList.add('topbar-scrolled')
+          }
+        } else {
+          selectHeader.classList.remove('header-scrolled')
+          if (selectTopbar) {
+            selectTopbar.classList.remove('topbar-scrolled')
+          }
+        }
+      }
+      window.addEventListener('load', headerScrolled)
+      onscroll(document, headerScrolled)
+    }
+  
+    /**
+     * Back to top button
+     */
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+      const toggleBacktotop = () => {
+        if (window.scrollY > 100) {
+          backtotop.classList.add('active')
+        } else {
+          backtotop.classList.remove('active')
+        }
+      }
+      window.addEventListener('load', toggleBacktotop)
+      onscroll(document, toggleBacktotop)
+    }
+  
+    /**
+     * Mobile nav toggle
+     */
+    on('click', '.mobile-nav-toggle', function(e) {
+      select('#navbar').classList.toggle('navbar-mobile')
+      this.classList.toggle('bi-list')
+      this.classList.toggle('bi-x')
+    })
+  
+    /**
+     * Mobile nav dropdowns activate
+     */
+    on('click', '.navbar .dropdown > a', function(e) {
+      if (select('#navbar').classList.contains('navbar-mobile')) {
+        e.preventDefault()
+        this.nextElementSibling.classList.toggle('dropdown-active')
+      }
+    }, true)
+  
+    /**
+     * Scrool with ofset on links with a class name .scrollto
+     */
+    on('click', '.scrollto', function(e) {
+      if (select(this.hash)) {
+        e.preventDefault()
+  
+        let navbar = select('#navbar')
+        if (navbar.classList.contains('navbar-mobile')) {
+          navbar.classList.remove('navbar-mobile')
+          let navbarToggle = select('.mobile-nav-toggle')
+          navbarToggle.classList.toggle('bi-list')
+          navbarToggle.classList.toggle('bi-x')
+        }
+        scrollto(this.hash)
+      }
+    }, true)
+  
+    /**
+     * Scroll with ofset on page load with hash links in the url
+     */
+    window.addEventListener('load', () => {
+      if (window.location.hash) {
+        if (select(window.location.hash)) {
+          scrollto(window.location.hash)
+        }
+      }
+    });
+  
+    /**
+     * Preloader
+     */
+    let preloader = select('#preloader');
+    if (preloader) {
+      window.addEventListener('load', () => {
+        preloader.remove()
+      });
+    }
+  
+  })()
+
+  var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+
+// Typewriter JS
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
